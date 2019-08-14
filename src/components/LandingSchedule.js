aa9670b841efd27641fea3fc9fafef0e7f5c109f
dangquangdon <dangquangdon@gmail.com>
2019-08-14 10:05:54 +0300

    add scss module

--- src/components/LandingSchedule.js
+++ src/components/LandingSchedule.js
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "../styles/homeSchedule.module.scss";
 
 const LandingSchedule = props => {
   return (
-    <div className="LandingSchedule">
+    <div className={styles.landingSchedule}>
       <div>
-        <p className="routeText blue">Business</p>
-        <p className="routeText red">Design</p>
-        <p className="routeText yellow">Tech</p>
+        <p className='routeText blue'>Business</p>
+        <p className='routeText red'>Design</p>
+        <p className='routeText yellow'>Tech</p>
       </div>
-      <div className="normalText">Lorem ipsum dono si amet</div>
+      <div className='normalText'>Lorem ipsum dono si amet</div>
       <button>learn more </button>
     </div>
   );
