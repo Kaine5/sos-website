dbd089c0ab1f1b0bc6e645e3ab4997adc976b5b5
dangquangdon <dangquangdon@gmail.com>
2019-08-14 11:25:53 +0300

    added the box

--- src/components/LandingSchedule.js
+++ src/components/LandingSchedule.js
@@ -1,16 +1,18 @@
 import React from "react";
-import styles from "../styles/homeSchedule.module.scss";
+import Box3d from "../components/Box3d";
 
 const LandingSchedule = props => {
   return (
-    <div className={styles.landingSchedule}>
+    <div className='LandingSchedule'>
       <div>
         <p className='routeText blue'>Business</p>
         <p className='routeText red'>Design</p>
         <p className='routeText yellow'>Tech</p>
       </div>
       <div className='normalText'>Lorem ipsum dono si amet</div>
-      <button>learn more </button>
+      <div style={{ marginLeft: "20px", position: "relative" }}>
+        <Box3d />
+      </div>
     </div>
   );
 };
