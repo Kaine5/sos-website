e484f26653e2d032089609deca9efd193ec5baf6
KagaK2 <KhanhPhan2512@gmail.com>
2019-08-15 12:13:59 +0300

    button draft

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import buttonStyles from "../styles/Buttons.module.scss";
 
 const Button = props => {
-  return <div className="btn">{props.name}</div>;
+  return (
+    <div className={buttonStyles.btnContainer}>
+      <div className={buttonStyles.btn}>{props.name}</div>
+    </div>
+  );
 };
 
 export default Button;
