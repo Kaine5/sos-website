2a97b49575ad847272be775ef1c2c545e3f4047d
dangquangdon <dangquangdon@gmail.com>
2019-08-15 11:40:39 +0300

    splite styles to module

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,59 +1,66 @@
 import React from "react";
+import styles from "../styles/Footer.module.scss";
+
 import { ReactComponent as FacebookLogo } from "../Facebook logo.svg";
 import { ReactComponent as InstagramLogo } from "../Instagram Logo.svg";
 import { ReactComponent as TwitterLogo } from "../Twitter logo.svg";
 import { ReactComponent as LinkedInLogo } from "../Linkedin logo.svg";
 import { ReactComponent as ShortcutLogo } from "../Shortcut Logo.svg";
-export default function Footer() {
+
+function Footer() {
   return (
-    <div className="footer">
-      <div className="flex-container">
-        <div className="flex-container-left">
+    <div className={styles.footer}>
+      <div className={styles.flexContainer}>
+        <div className={styles.flexContainerLeft}>
           {/* <div>
             <img className="shortcutlogo" src="/img/shortcutlogo.png" />
           </div> */}
-          <ShortcutLogo className="shortcutlogo" />
-          <div className="flex-container-media">
+          <ShortcutLogo className={styles.shortcutlogo} />
+          <div className={styles.flexContainerMedia}>
             <div>
-              <FacebookLogo className="logo" />
+              <FacebookLogo className={styles.logo} />
             </div>
             <div>
-              <InstagramLogo className="logo" />
+              <InstagramLogo className={styles.logo} />
             </div>
             <div>
-              <TwitterLogo className="logo" />
+              <TwitterLogo className={styles.logo} />
             </div>
             <div>
-              <LinkedInLogo className="logo" />
+              <LinkedInLogo className={styles.logo} />
             </div>
           </div>
-          <p className="info-left">
+          <p className={styles.infoLeft}>
             ©2019 The Shortcut. Terms of Service and Data Description{" "}
           </p>
-          <p className="info-left">
+          <p className={styles.infoLeft}>
             Lapinlahdenkatu 16 (Entrance 15B), 00180 Helsinki
           </p>
         </div>
 
-        <div className="flex-container-right">
-          <div className="subscribe">
-            <p className="newsletter">Subsribe to our newsletter</p>
-            <div className="flex-container-input">
+        <div className={styles.flexContainerRight}>
+          <div className={styles.subscribe}>
+            <p className={styles.newsletter}>Subsribe to our newsletter</p>
+            <div className={styles.flexContainerInput}>
               <input
-                className="text"
-                type="text"
-                placeholder="Your email"
-                name="school"
+                className={styles.text}
+                type='text'
+                placeholder='Your email'
+                name='school'
               />
-              <input className="go" type="submit" value="Go" />
+              <input className={styles.go} type='submit' value='Go' />
             </div>
           </div>
 
-          <input className="contact" type="submit" value="Contact us" />
-          <p className="info">info@theshortcut.org | (+358) 46 571 2720 </p>
-          <p className="info"> Learn about our Privacy Policies</p>
+          <input className={styles.contact} type='submit' value='Contact us' />
+          <p className={styles.info}>
+            info@theshortcut.org | (+358) 46 571 2720{" "}
+          </p>
+          <p className={styles.info}> Learn about our Privacy Policies</p>
         </div>
       </div>
     </div>
   );
 }
+
+export default Footer;
