b5b1f974cf7eebfa337b71867e0f5879c1662669
Alina Ilina <alinailinet@gmail.com>
2019-08-16 12:23:37 +0300

    Changes

--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -1,32 +1,34 @@
 import React from "react";
 import Header from "../components/Header";
 
+const element = <h1>Hello!</h1>
+
 function GetInvolved() {
   return (
+    
     <div className="App">
       <Header />
+      
 
       <div className="container">
-        <div className="item-1">
-          <h1 className="topic">
-            <span className="green">Business</span> <br />
-            <span className="red">Design</span> <br />
-            <span className="yellow">Tech</span> <br />
-          </h1>
-        </div>
 
-        <div class="item-2">
+        
+
+          <div className="item-1">
           <p className="paragrath">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <div className="button">
-            learn<br />
-            more
           </div>
-        </div>
+
+          <div className="item-2">
+          </div>
+
+         
       </div>
-    </div>
+
+      </div>
+   
   );
 }
 
