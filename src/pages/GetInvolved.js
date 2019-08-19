37642c2d740b7d505d6471209618ddf58a3aa172
Alina Ilina <alinailinet@gmail.com>
2019-08-19 14:25:13 +0300

    getinvolved page done

--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -1,34 +1,49 @@
 import React from "react";
+import CanvasContent from "../components/CanvasContent";
 import Header from "../components/Header";
 
-const element = <h1>Hello!</h1>
+import Box3d from "../components/Box3d";
+import styles from "../styles/GetInvolved.module.scss";
+import Button from "../components/Button";
 
-function GetInvolved() {
-  return (
-    
-    <div className="App">
-      <Header />
-      
 
-      <div className="container">
 
-        
+const GetInvolved = props => {
+    return (
+      <div className={styles.getInvolved}>
+        <Header />
 
-          <div className="item-1">
-          <p className="paragrath">
+        <CanvasContent>
+
+          <p className={styles.item1}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
+ 
+          <div className={styles.item2} >
+
+            <div className={styles.btnImage}>
+              <Button height="15vw" width="15vw" />
+              <img src="/img/btn-illustration1.png"></img>
+            </div>
+
+            <div className={styles.btnImage} >
+              <Button height="15vw" width="15vw" />
+              <img src="/img/btn-illustration2.png"  ></img>
+            </div>
+
+            <div className={styles.btnImage} >
+              <Button height="15vw" width="15vw"/>
+              <img src="/img/btn-illustration3.png"></img>
+            </div>
+
           </div>
-
-          <div className="item-2">
-          </div>
-
-         
-      </div>
-
-      </div>
+          
    
+        
+      </CanvasContent>   
+    </div>
+
   );
 }
 
