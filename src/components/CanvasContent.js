16c9d06f5e5d74c9b85b3a96f00f0acba6b0dcb2
KagaK2 <KhanhPhan2512@gmail.com>
2019-08-20 11:42:53 +0300

    added Proptypes and button draft

--- src/components/CanvasContent.js
+++ src/components/CanvasContent.js
@@ -1,11 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const CanvasContent = React.forwardRef((props, ref) => {
   return (
-    <div className='Canvas' ref={ref} onScroll={props.onScroll}>
-      <div className='CanvasContent'>{props.children}</div>
+    <div className="Canvas" ref={ref} onScroll={props.onScroll}>
+      <div className="CanvasContent">{props.children}</div>
     </div>
   );
 });
+
+CanvasContent.propTypes = {
+  onScroll: PropTypes.func
+};
 
 export default CanvasContent;
