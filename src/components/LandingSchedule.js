0905ad7f438ba05326bef9fbedfdf3fb55d34cac
Alina Ilina <alinailinet@gmail.com>
2019-08-30 15:41:55 +0300

    Landing schedule resp

--- src/components/LandingSchedule.js
+++ src/components/LandingSchedule.js
@@ -12,13 +12,14 @@ const LandingSchedule = () => {
       </div>
 
       <div className={styles.item2}>
-        <p>2 Days. 48+ hands-on workshops </p>
-        <p>For entrepreneurs and startups </p>
-        <p> Learn from the doers</p>
+        <p>2 Days.<br></br>
+          48+ hands-on workshop <br></br>
+          For entrepreneurs and startups <br></br>
+          Learn from the doers</p>
       </div>
       <div className={styles.item3}>
         <div className={styles.button}>
-          <Button height="8vmax" width="24vmax" name="learn more" />
+          <Button height="10vmax" width="24vmax" name="learn more" />
         </div>
       </div>
     </div>
