070ee1752a1d59a76f4003140519c5bc7796b0b4
KagaK2 <KhanhPhan2512@gmail.com>
2019-09-11 12:10:20 +0300

    added text

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -110,6 +110,12 @@ const Schedule = () => {
         display="timeline"
       />
       <CanvasContent hasBorder="true">
+        <p className={styles.infoText}>
+          The Event will be held in the Maria01, Lapinlahdenkatu 16. <br />
+          Workshops are divided into 5 spaces inside the Maria01: <br />
+          The Shortcut, Vertical, Icebreakers, Maria01 event space, Games
+          Factory
+        </p>
         <div className={styles.ScheduleList}>
           {currentSchedule === "5th"
             ? daySchedule(data.firstDay)
