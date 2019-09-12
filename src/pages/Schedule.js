06cf586ee57f84109805a7c6834cacc831414aea
KagaK2 <KhanhPhan2512@gmail.com>
2019-09-12 16:15:16 +0300

    new schedule text

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -111,7 +111,7 @@ const Schedule = () => {
       />
       <CanvasContent hasBorder="true">
         <p className={styles.infoText}>
-          The Event will be held in the Maria01, Lapinlahdenkatu 16. <br />
+          The Event will be held in Maria01, Lapinlahdenkatu 16. <br />
           Workshops are divided into 5 spaces inside the Maria01: <br />
           The Shortcut, Vertical, Icebreakers, Maria01 event space, Games
           Factory
