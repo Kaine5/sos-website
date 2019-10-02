ae24b9dfc45605b6638aafcf6a361d83ce60322c
dangquangdon <dangquangdon@gmail.com>
2019-10-02 10:17:38 +0300

    add og metadata

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ const App = () => {
           name='keywords'
           content='School of Startups, SoS, Helsinki startup events, The Shortcut SoS, Business, Design, Tech'
         />
+        <meta property='og:title' content='School of Startups' />
+        <meta property='og:image' content='/img/sos-background.png' />
+        <meta
+          property='og:description'
+          content='School of Startups (SoS) is a two-day event in Helsinki for startup doers planning on scaling and for talent eager to improve their employability. Filled with 40 workshops covering business, tech, and design topics given by startup doers, SoS is one of the best in Helsinki startup events scene to bring together startup pros, experts and enthusiasts to one platform.'
+        />
+        <meta property='og:url' content='https://schoolofstartups.net/' />
       </Helmet>
       <Router>
         <Route path='/' exact component={Home} />
